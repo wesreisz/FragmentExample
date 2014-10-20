821a39c9d0bd7509218a2a7c9f4b291cd59b8973
Wesley Reisz <wes@wesleyreisz.com>
2014-10-20 16:50:38 -0400

    step 2

--- app/src/main/java/com/wesleyreisz/myapplication/FragmentA.java
+++ app/src/main/java/com/wesleyreisz/myapplication/FragmentA.java
@@ -1,9 +1,17 @@
 package com.wesleyreisz.myapplication;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by wesleyreisz on 10/20/14.
  */
 public class FragmentA extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
