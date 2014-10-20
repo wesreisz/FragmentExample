1a4d28ee4d1e6eb838155555ffdbf1ee77834b82
Wesley Reisz <wes@wesleyreisz.com>
2014-10-20 17:07:40 -0400

    step 2

--- app/src/main/java/com/wesleyreisz/myapplication/FragmentA.java
+++ app/src/main/java/com/wesleyreisz/myapplication/FragmentA.java
@@ -5,13 +5,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by wesleyreisz on 10/20/14.
  */
-public class FragmentA extends Fragment {
+public class FragmentA extends Fragment implements AdapterView.OnItemClickListener {
+    ListView listView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView= (ListView) getActivity().findViewById(R.id.listView);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.titles, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
